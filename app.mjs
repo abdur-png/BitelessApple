a1adc0377e85dc5b24d5d037d250c82ef54b4a0f
Abdur Rahman <ar7165@linserv1.cims.nyu.edu>
2023-11-04 20:34:08 -0400

    done

--- app.mjs
+++ app.mjs
@@ -8,6 +8,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT ?? 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
