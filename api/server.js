74b254774a0d275717bc7909c741b11ac6563f1b
Abdur Rahman <ar7165@nyu.edu>
2023-11-08 15:17:53 -0500

    fq

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect("mongodb://ar7165:49fRMJTK@class-mongodb.cims.nyu.edu/ar7165",{
+    useNewURLParser:true,
+    useUnitedTopology:true
+})  
+    .then(() => console.log("Connected to DB"))
+    .catch(console.err);
+
+app.listen(21667, ()=> console.log(`Server listening on port 21667`));
