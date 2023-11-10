62485a7cf8759a1cd77e8b63dad9f3aa180f9490
Abdur Rahman <ar7165@nyu.edu>
2023-11-09 22:21:08 -0500

    milestone 2 done

--- api/db.js
+++ api/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const mongoURI = process.env.DSN; // Assuming you store your MongoDB URI in an environment variable for security
 
